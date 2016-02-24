10ad7e7383cab08e1269db76ad363b784b707aeb
Thomas <jtruant@gmx.com>
2016-02-23 18:09:48 -0700

    Hello Express/Mongo World!

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require("express");
-
+var app = express();
+app.set('port', (process.env.PORT || 5000));
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
+
 var url = require('./config').url;
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+app.get('/', function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to mongodb server.");
+    res.end("Greetings from MongoDB // Express.js");
+    db.close();
+  });
+});
+app.listen(app.get('port'), function() {
+  console.log("short-url app listening on port: " + app.get('port'));
 });
